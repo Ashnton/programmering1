160f2ee73a33d6451b6a2d59870b1d24ebdf4b3d
Ashnton <antonlidstrom71@gmail.com>
2023-08-22 15:58:07 +0200

    Skapade första testfil

---
+++ myfile.py
@@ -0,0 +1,14 @@
+print ('Hello world')
+
+number = 1
+
+print (number)
+
+
+def my_function():
+    for i in range(7):
+        if i != 5:
+            print(i)
+        else:
+            print(i-4)
+my_function()
