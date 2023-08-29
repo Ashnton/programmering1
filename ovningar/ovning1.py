b0b6d2e60b830d4c063e69eae9395b3b53f7aa27
Ashnton <antonlidstrom71@gmail.com>
2023-08-29 11:11:22 +0200

    Övning om print gjord

---
+++ ovningar/ovning1.py
@@ -0,0 +1,15 @@
+# Bestämmer mitt namn
+name = "Anton"
+
+# Räknar ut min ålder i timmar
+age = 17
+age_in_hours = age * 365.25 * 24
+
+# Bestämmer min favoritmaträtt
+best_food = "Kyckling med cashewnötter (thaistyle)"
+
+# Skriver ut informationen
+print("Hej mitt namn är "+name+". Jag är "+str(age_in_hours)+" timmar gammal och min favoritmaträtt är "+best_food+".")
+
+# Vad har print för syfte vid programmering?
+# Syftet med print är att enkelt kunna visa utdata från programmet på skärmen vilket är användbart vid till exempel debugging men även för mycket annat.
---
+++ ovningar/print.py
@@ -0,0 +1,58 @@
+# 1
+# a)
+print(12-7)
+
+# b)
+print(24/3 + 9)
+
+# c) 
+print(3*43-23)
+
+# 2
+print('Hej! Hur mår du?')
+
+# 3
+# a)
+print(15+15)
+
+# b)
+print(15+15+29)
+
+# c)
+print(3*(15+29+12))
+
+# 4
+print(219*45+8)
+
+# 5
+# b)
+print("Om du kör med hastigheten 70 km/h i 1,5 timmar, så hinner du " + str(70*1.5) + " km.")
+
+# 6
+for i in range (10):
+    print("hej")
+
+# 7
+# 1 = A
+# 2 = D
+# 3 = C
+# 4 = B
+
+# 8
+# b)
+print(3+2*10)
+print (5 * (8-7))
+
+# 9
+# a)
+print(2**2)
+print(3**2)
+print(4**2)
+# Den betyder "upphöjt till" alltså det som kommer efter är en exponent
+
+# 10
+# a)
+print(9/2, "och", 9//2)
+print(11/6, "och", 11//6)
+print(21/8,"och", 21//8)
+# Symbolen // gör så att svaret alltid avrundas neråt, så kallad floor division
